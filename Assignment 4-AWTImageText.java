9d77c7cc484d9077554f955169f191151f95874a
hemantraj-x <hemantraj1104@gmail.com>
2026-05-13 17:58:46 +0530

    Add files via upload

---
+++ Assignment 4-AWTImageText.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+
+public class AWTImageText extends Frame {
+
+    Image img;
+
+    AWTImageText() {
+
+        setTitle("AWT Example");
+        setSize(400,300);
+        setVisible(true);
+
+        img = Toolkit.getDefaultToolkit().getImage("image.jpg");
+    }
+
+    public void paint(Graphics g) {
+
+        g.drawString("Hello AWT Application",150,80);
+
+        g.drawImage(img,120,120,100,100,this);
+    }
+
+    public static void main(String args[]) {
+
+        new AWTImageText();
+    }
+}
